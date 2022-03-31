6e8036797ebb40daad2cc68e944172138e036da7
WilliamF <williamferreira132003@gmail.com>
2022-03-30 23:56:19 -0300

    feat: start project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="testText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+    in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
+    PageMaker including versions of Lorem Ipsum.</div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+.app {
+  position: relative;
+}
+
+.testText {
+  width: 75%;
+  align-self: center;
+}
+
+html,
+body {
+  height: 100%;
+  background-color: #191622;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #FFFFFF;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+</style>
