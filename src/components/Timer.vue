90ed9533bc9caf7afb0ba4a08e80645a846bef29
WilliamF <williamferreira132003@gmail.com>
2022-03-31 23:35:08 -0300

    feat: implement first countdown

---
+++ src/components/Timer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+
+const minutes = ref<string | number>('0')
+const seconds = ref<string | number>('5')
+
+function startTimer() {
+    const handleTimer = setInterval(() => {
+        seconds.value = Number(seconds.value)
+        minutes.value = Number(minutes.value)
+        seconds.value = seconds.value - 1
+
+        if (seconds.value === 0 && minutes.value !== 0) {
+            minutes.value = minutes.value - 1
+            seconds.value = 59
+        }
+
+        if (!seconds.value) {
+            clearTimerInterval(handleTimer)
+        }
+    }, 1000)
+}
+
+function clearTimerInterval(intervalName: any) {
+    clearInterval(intervalName)
+}
+
+startTimer()
+
+function test() {
+    console.log('sad')
+}
+
+</script>
+
+<template>
+    <p @click="test()">{{ minutes }}:{{ seconds }}</p>
+</template>
