37c1009cc31afe2c6794401b3c9940d3333dcf9a
WilliamF <williamferreira132003@gmail.com>
2022-04-07 18:43:23 -0300

    feat: finish text digitation

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from '@vue/reactivity'
 
-const baseText = ref('oh-yes-boy')
+const baseText = ref('|oh yes boy')
 
 const text = ref(baseText.value)
 const textDisplay = ref(text.value)
@@ -18,23 +18,25 @@ function startText(verifier: boolean) {
 
         const typedLetter = event.key
 
+        const fromIndex = splitedText.value.indexOf('|')
+
         if (typedLetter === 'Backspace') {
             currentIndex.value = currentIndex.value - 1
-            splitedText.value.splice(currentIndex.value, 1, `${baseText.value.split('')[currentIndex.value]}`)
+
+            splitedText.value.splice(fromIndex, 1)
+            splitedText.value.splice(currentIndex.value, 0, '|')
+
+            splitedText.value.splice(currentIndex.value + 1, 1, `${baseText.value.split('')[currentIndex.value + 1]}`)
+
 
             textDisplay.value = splitedText.value.join('')
-            return
+
+            return        
         }
 
-        if(currentIndex.value === baseText.value.length) {
-            const allCorrect = document.getElementsByClassName('red')
-            if (allCorrect.length === 0) {
-                emit('finished')
-            } else {
-                return
-            }
-        }
-        
+        splitedText.value.splice(fromIndex, 1)
+        splitedText.value.splice(currentIndex.value + 1, 0, '|')
+
         if (typedLetter !== splitedText.value[currentIndex.value]) {
             splitedText.value.splice(currentIndex.value, 1, `<span class='red'>${splitedText.value[currentIndex.value]}</span>`);
         } else {
@@ -43,6 +45,15 @@ function startText(verifier: boolean) {
 
         textDisplay.value = splitedText.value.join('')
         currentIndex.value++
+
+        if(currentIndex.value === baseText.value.length -1) {
+            const allCorrect = document.getElementsByClassName('red')
+            if (allCorrect.length === 0) {
+                emit('finished')
+            } else {
+                return
+            }
+        }
     }
 }
 
